07f671357fe23a7327365b2bd77d4a3328eba239
noah-schroeder <159392669+noah-schroeder@users.noreply.github.com>
2024-07-05 16:04:25 -0700

    script update

---
+++ runSMAlocal.R
@@ -0,0 +1,12 @@
+#How to use this file:
+##Highlight all code (for example, on a PC, just hit ctrl+a to select all).
+##With all of the code highlighted, click "Run App" in the top right of Rstudio.
+###This will pull reference files from GitHub, install/update packages, and launch the program locally.
+###Please be patient, it may take a minute or two. 
+
+# Source the script from GitHub
+source("https://raw.githubusercontent.com/noah-schroeder/simple_meta-analysis/main/run_app.R")
+
+# Call the function to run the app
+runApp()
+
